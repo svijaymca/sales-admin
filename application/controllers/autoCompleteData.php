0fe68014854af9542c9fcbcf1915a7b5ce1e4d98
VIJAY S <34551022+svijaymca@users.noreply.github.com>
2018-03-24 00:58:05 +0530

    Update autoCompleteData.php

--- application/controllers/autoCompleteData.php
+++ application/controllers/autoCompleteData.php
@@ -14,18 +14,42 @@ class autoCompleteData extends CI_Controller {
 		
 	}
 
-	public function getSupplier()
-	{	
+	public function getSupplier(){
+
 		$keyword = $this->input->get('term');
 		$query = $this->autoCompleteModel->gerSupplierData($keyword); 
 			if(!empty($query)){
-				$data = array(); //Create array  
+				$data = array(); 
 				foreach( $query as $row )  
 				{  
 					$data[] = array(   
 									'supplierId'=>$row->supplierId,  
 									'value' => $row->supplierName,
 									'label' => $row->supplierName.'-'.$row->supplierCode
+								);    
+				} 
+			 echo json_encode($data);
+			}else{
+				echo "No Data Found";
+			}
+			
+		
+	}
+	public function getProduct(){
+		
+		$keyword = $this->input->get('term');
+		$query = $this->autoCompleteModel->gerProductData($keyword); 
+			if(!empty($query)){
+				$data = array();   
+				foreach( $query as $row )  
+				{  
+					$data[] = array(   
+									'label' => $row->productName.' [ '.$row->productCode.' ]',
+									'value' => $row->productName,
+									'productId'=>$row->productId,  
+									'productCode' => $row->productCode,
+									'productRate' => $row->productRate
+									
 								);    
 				} 
 			 echo json_encode($data);
