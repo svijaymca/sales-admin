c49010da8b194ee4c07ac9bcfe504950552f128e
VIJAY S <34551022+svijaymca@users.noreply.github.com>
2018-03-24 00:58:47 +0530

    Update autoCompleteModel.php

--- application/models/autoCompleteModel.php
+++ application/models/autoCompleteModel.php
@@ -9,6 +9,14 @@ class autoCompleteModel extends CI_Model{
 			//echo $this->db->last_query(); exit;
         return $query->result(); 
 	}
+	public function gerProductData($term){
+
+			$this->db->select('*')->from('products'); 
+			$this->db->where("productStatus = 0 AND (productName LIKE '%".$term."%' OR productCode LIKE '%".$term."%' ) ");
+			$query = $this->db->get();     
+			//echo $this->db->last_query(); exit;
+        return $query->result(); 
+	}
 
 }
 ?>
