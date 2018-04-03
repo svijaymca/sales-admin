ad9aea2c7ffe5784d5d25f48841708efb0fe13e7
VIJAY S <34551022+svijaymca@users.noreply.github.com>
2018-04-04 00:38:17 +0530

    Update invoiceModel.php

--- application/models/invoiceModel.php
+++ application/models/invoiceModel.php
@@ -32,7 +32,7 @@ class invoiceModel extends CI_Model{
 		$id   = $this->UtilityMethods->getId('invoiceId','invoice','invoiceUniqId', $this->uri->segment(3) );
 
 		$where1 = array( "invoiceStatus"=>0, "invoiceId"=>$id );
-			$this->db->select('A.*, customerName, branchName');
+			$this->db->select('A.*, customerName, branchName, customerAddress');
 			$this->db->from('invoice A');
 			$this->db->where($where1);
 			$this->db->join('customers', 'customers.customerId = A.invoiceCustomerId', 'left');
@@ -64,7 +64,7 @@ class invoiceModel extends CI_Model{
 
 		$this->db->set($data);
 		$this->db->where($where);
-		$this->db->update('invoiceDetails'); echo $this->db->last_query(); exit;
+		$this->db->update('invoiceDetails'); 
 		$status = $this->db->trans_status();
 		return array($where['invoiceDetailsId'], $status);
 	}
@@ -84,6 +84,30 @@ class invoiceModel extends CI_Model{
 		}
 
 	} 
+
+	public function editInvoicePrint($id){
+		
+		
+
+		$where1 = array( "invoiceStatus"=>0, "invoiceId"=>$id );
+			$this->db->select('A.*, customerName, branchName');
+			$this->db->from('invoice A');
+			$this->db->where($where1);
+			$this->db->join('customers', 'customers.customerId = A.invoiceCustomerId', 'left');
+			$this->db->join('branch', 'branch.branchId = A.invoiceBranchId', 'left');
+			$query1 = $this->db->get()->row();
+
+		$where2 = array( "invoiceDetailsStatus"=>0, "invoiceDetailsInvoiceId"=>$id );
+			$this->db->select('B.*, productName, productCode');
+			$this->db->from('invoiceDetails B');
+			$this->db->where($where2);
+			$this->db->join('products', 'products.productId = B.invoiceDetailsProductId', 'left');
+			$query2 = $this->db->get()->result();
+
+				
+
+		return array($query1, $query2);
+	}
  
 
 
