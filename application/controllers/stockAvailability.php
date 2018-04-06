2dfcef2de2aca2849f74ce42122159e0b83354e5
VIJAY S <34551022+svijaymca@users.noreply.github.com>
2018-04-06 23:49:41 +0530

    Update stockAvailability.php

--- application/controllers/stockAvailability.php
+++ application/controllers/stockAvailability.php
@@ -6,6 +6,7 @@ class StockAvailability extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->model('UtilityMethods');
+		$this->UtilityMethods->loginAuthentication();
 	}
 
 	public function index()
